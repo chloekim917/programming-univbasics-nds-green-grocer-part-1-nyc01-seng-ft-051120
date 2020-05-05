7ec4f0b2717e86a02c2eda8c093fb90ed66a8c4f
Seoyoung Kim <mintholic1@gmail.com>
2020-05-05 20:49:37 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,7 +11,7 @@ end
 def consolidate_cart(cart)
   my_cart = []
   cart.each do |things|
-    if my_cart{things}.nil?
+    if my_cart.include? 
       things[:count] = 1
       my_cart.push(things)
     else 

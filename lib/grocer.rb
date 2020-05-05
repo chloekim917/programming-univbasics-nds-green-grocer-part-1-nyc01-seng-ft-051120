bd7c5e5d1a266e6b51f3b99f4185b5d181bc1845
Seoyoung Kim <mintholic1@gmail.com>
2020-05-05 20:23:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,6 +9,16 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  my_cart = find_item_by_name_in_collection(cart)
-  cart.each do |things|
-    if things[:item] == my_cart[:]
+ new_hash = {}
+ cart.each do |things|
+   things.each do |name, price_hash|
+     if new_hash[name].nil?
+       price_hash[:count] = 1
+       new_hash[name] = price_hash
+     else 
+       new_hash[name][:count] += 1
+     end
+   end
+ end
+ new_hash
+ end

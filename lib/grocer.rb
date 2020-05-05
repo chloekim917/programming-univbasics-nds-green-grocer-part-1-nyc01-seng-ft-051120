7f2604599f735b471e4ac487f009c513706773cf
Seoyoung Kim <mintholic1@gmail.com>
2020-05-05 20:25:01 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,7 +13,7 @@ def consolidate_cart(cart)
  cart.each do |things|
    things.each do |name, price_hash|
      if new_hash[name].nil?
-       price_hash[:count] = 1
+       price_hash[:count]
        new_hash[name] = price_hash
      else 
        new_hash[name][:count] += 1

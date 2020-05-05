37e35203bd8bb28b6b87b494fba9f1c6e9633fb6
Seoyoung Kim <mintholic1@gmail.com>
2020-05-05 19:18:19 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,8 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do |things|
     if things[:item] == name
       return things
+    else 
+      return nil 
     end
   end
 end

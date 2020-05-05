8f480733da7fa23606fd1c0c6b7f61a429f60466
Seoyoung Kim <mintholic1@gmail.com>
2020-05-05 19:36:16 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
   collection.each do |things|
     if things[:item] == name
@@ -12,6 +13,7 @@ def consolidate_cart(cart)
   cart.each do |things|
     if things[:item] == name
       my_cart.push(things)
+      binding.pry
     end
   end
   return my_cart
